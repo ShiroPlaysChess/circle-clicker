f9087413668a61c74f718566e51bb6b26db89855
Dinesh Lal <d.lal@EDU.epsb.ca>
2023-11-27 14:46:42 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById('aimCanvas');
+    const ctx = canvas.getContext('2d');
+    
+    const circles = [];
+    const circleCount = 10;
+    const circleRadius = 20;
+    
+    for (let i = 0; i < circleCount; i++) {
+      const circle = {
+        x: Math.random() * (canvas.width - 2 * circleRadius) + circleRadius,
+        y: Math.random() * (canvas.height - 2 * circleRadius) + circleRadius,
+        dx: (Math.random() - 0.5) * 4,
+        dy: (Math.random() - 0.5) * 4,
+        color: '#' + Math.floor(Math.random() * 16777215).toString(16), // Random color
+      };
+      circles.push(circle);
+    }
+    
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+      circles.forEach(circle => {
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circleRadius, 0, Math.PI * 2);
+        ctx.fillStyle = circle.color;
+        ctx.fill();
+        ctx.closePath();
+        
+        circle.x += circle.dx;
+        circle.y += circle.dy;
+        
+        if (circle.x - circleRadius < 0 || circle.x + circleRadius > canvas.width) {
+          circle.dx *= -1;
+        }
+        if (circle.y - circleRadius < 0 || circle.y + circleRadius > canvas.height) {
+          circle.dy *= -1;
+        }
+      });
+      
+      requestAnimationFrame(draw);
+    }
+    
+    canvas.addEventListener('click', function(event) {
+      const rect = canvas.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
+      
+      for (let i = 0; i < circles.length; i++) {
+        const circle = circles[i];
+        const distance = Math.sqrt((mouseX - circle.x) ** 2 + (mouseY - circle.y) ** 2);
+        if (distance < circleRadius) {
+          circles.splice(i, 1); // Remove the clicked circle from the array
+          break;
+        }
+      }
+    });
+    
+    draw();
